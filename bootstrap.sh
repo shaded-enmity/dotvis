7c1c34c32e77d5f918c4a17263b9b2105d9ea564
Pavel Odvody <podvody@redhat.com>
2017-11-09 15:08:31 +0100

    Print more helpful message and disable CORS

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+ADDR=${DOTVIS_SERVE_HOST:-localhost}
 name=$1
 echo "Project name: $name"
 tmpfile=$(mktemp /tmp/layout.XXXXXX)
 cat -> $tmpfile
+sed -i "13i\ \ disableHostCheck: true," /opt/dotvis/pm/dev-server.js
+echo "export default { dataUrl: '//$ADDR:8000/' };" > /opt/dotvis/pm/src/config.js
 mkdir -p /opt/dotvis/data-stage/
 cd /opt/dotvis/dot2pm && node layout.js $tmpfile
 rm $tmpfile
@@ -14,6 +17,6 @@ cp /opt/dotvis/data-stage/* /opt/dotvis/data/$name/undefined
 cp /opt/dotvis/dot2pm/data/* /opt/dotvis/data/$name/undefined
 echo '{"latest": "'$name'"}' > /opt/dotvis/data/$name/manifest.json
 cd /opt/dotvis/data && python3 serve.py 8000 &
-echo "Serving ... now access your graph at http://localhost:8081/#/galaxy/$name"
+echo "Serving ... now access your graph at http://$ADDR:8081/#/galaxy/$name"
 cd /opt/dotvis/pm && npm build
 exec npm start 1>/dev/null
